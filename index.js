c03df3287e39d621d5af4b0618a72c27f05dc2f5
Matthew Tran <matt.h.tran@gmail.com>
2023-10-03 15:20:51 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(action = "roller-skate"){
+    return `This Saturday, I want to ${action}!`;
+}
+saturdayFun("bathe my dog");
+
+function mondayWork(action = "go to the office"){
+    return `This Monday, I will ${action}.`;
+}
+
+mondayWork("work from home");
+
+function wrapAdjective(sign = "*") {
+    return function (adjective = "special") {
+        return `You are ${sign}${adjective}${sign}!`;
+    };
+
+}
